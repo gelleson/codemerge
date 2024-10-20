0bdef4d24d152568fb93d47158ba3f29f0cbb936
Altynbek Kaliakbarov <go.gelleson@gmail.com>
2024-10-20 22:49:54 +0500

    fix: Update version and documentation for new features in CodeMerge

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use rayon::prelude::*;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "codemerge")]
 enum CodeMerge {
-    #[structopt(name = "merge")]
+    #[structopt(name = "merge", about = "Merge multiple code files into a single output file")]
     Merge {
         #[structopt(short, long, parse(from_os_str))]
         output: Option<PathBuf>,
@@ -24,7 +24,7 @@ enum CodeMerge {
         #[structopt(short, long)]
         verbose: bool,
     },
-    #[structopt(name = "tokens")]
+    #[structopt(name = "tokens", about = "Calculate token counts for multiple code files")]
     Tokens {
         #[structopt(short, long, default_value = "10")]
         count: usize,
